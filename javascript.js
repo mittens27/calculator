bb5ec6f9bfcc494c915ee6804af5994089080d86
Andy <amluck88@gmail.com>
2024-02-25 19:55:44 -0500

    added logic for add, sub, div, mult

--- javascript.js
+++ javascript.js
@@ -22,21 +22,60 @@ var operators = {
     divide: (a, b) => a / b
 };
 
-const addBtn = document.getElementById("add");
-const subBtn = document.getElementById("subtract");
-const divBtn = document.getElementById("divide");
-const mulBtn = document.getElementById("multiply");
+const addBtn = document.getElementById("#add");
+const subBtn = document.getElementById("#subtract");
+const divBtn = document.getElementById("#divide");
+const mulBtn = document.getElementById("#multiply");
 
-const opBtns = document.querySelectorAll(".opBtn");
+const opBtns = document.querySelector(".operators");
 
-opBtns.forEach(opBtn => {
-    opBtn.addEventListener("click", () => {
-        var a = display.value;
+opBtns.addEventListener("click", function(event) {
+
+    const clickedBtn = event.target;
+
+    if (clickedBtn.tagName === "BUTTON") {
+        console.log("you clicked an op button");
+        aStr = display.value;
         display.value = "0";
-    });
+
+        switch (clickedBtn.id) {
+            case "add":
+                operator = "add";
+                break;
+            case "subtract":
+                operator = "subtract";
+                break;
+            case "divide":
+                operator = "divide";
+                break;
+            case "multiply":
+                operator = "multiply";
+                break;
+        }
+    }
 });
 
+const eqBtn = document.querySelector("#equals");
 
+eqBtn.addEventListener("click", () => {
+
+    var bStr = display.value;
+    a = parseInt(aStr, 10);
+    b = parseInt(bStr, 10);
+    if (operator === "add") {
+        var results = operators.add(a, b);
+        display.value = results;
+    } else if (operator === "subtract") {
+        var results = operators.subtract(a, b);
+            display.value = results;
+    } else if (operator === "divide") {
+        var results = operators.divide(a, b);
+        display.value = results;
+    } else if (operator === "multiply") {
+        var results = operators.multiply(a, b);
+        display.value = results;
+    }
+});
 
 const clear = document.getElementById("clear");
 
